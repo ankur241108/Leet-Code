d56b1ff1eccdda9c5f4d3038ee4bce2f8d4a5e54
ankur241108 <ankursingh241108@gmail.com>
2026-09-07 19:26:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (77.9%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,22 +4,36 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        int ans=0 ;
+        int ans=n;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                return ans;
-            }
+        //     if(nums[mid]==target){
+        //         ans=mid;
+        //         return ans;
+        //     }
 
-           else  if(nums[mid]<target){
-                ans=mid+1;
-                low=mid+1;
-            }
+        //    else  if(nums[mid]<target){
+        //         ans=mid+1;
+        //         low=mid+1;
+        //     }
 
-            else{
-                high=mid-1;
+        //     else{
+        //         high=mid-1;
+        //     }
+
+
+
+
+
+        if (nums[mid] >= target) {
+                // Potential answer found, try to go left
+                ans = mid;
+                high = mid - 1;
+            } else {
+                // Go right
+                low = mid + 1;
             }
+        
         }
         return ans;
     }
